2ebdadf32b1d73eeda320ec1a6a325bcef44a765
ArturoHernandez26 <159203492+ArturoHernandez26@users.noreply.github.com>
2024-03-15 14:57:01 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -12,7 +12,20 @@ then(data=>{
         //Que hace? Aqui se crea un elemento h2,se asigna el nombre de la categoria como su contenido de texto , y luego se añade este titulo al contenedor menu/.
         categoryTitle.textContent= category.category;
         menuContainer.appendChild(categoryTitle);
+        const table = document.createElement('table');
+        //Que hace? se crea un elemento table para cada categoria. Ademas, se prepara el encabezado (tableHead)con las columnas pertinentes.tablebody se inicializa vacio y se llenara con los elementos de la categoria.
+        const tableHead=´<tr><th>foto</th><th>Descricion</th><th>Precio</th></tr>´;
+        let tablebody = '';
+        //Como funciona? Para toda categoria en los datos , se realizan varios pasos Crear un titulo para la categoria (se crea un elemento <h2> para el titulo de la categoria,se establece su contenido de texto al nombre de la categoria(category.category),luego se agrega al contenedor del menu).
+        //Crear una tabla para los elementos de esas categorias: se genera una tabla por cada categoria(primero se define el encabezado de la tabla <tr><th>foto</th><th>Descricion</th><th>Precio</th></tr> ).
         
-const table = document.createElement('table');
+        category.items.forEach item =>{
+            tableBody +=´<tr>
+              <td><img src="{item.image}alt="${item.name}"</td>
+              <td>$(item.name)="${item.description}"</td>
+              <td>${item.price}</td>  
+            </tr>´;
+        };
     });
 })
+
