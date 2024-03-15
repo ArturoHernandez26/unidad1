ebfc59efe8879fccb575234a43ddacfd5653b1fa
ArturoHernandez26 <159203492+ArturoHernandez26@users.noreply.github.com>
2024-03-15 14:14:26 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,3 +4,11 @@ fetch('menu.json')
 .then(response => response.json())
 // que hace? Esta linea toma el objeto de respuesta en un objeto de respuesta en un obtenido del fetch y utiliza el metodo .json() para convertir el cuerpode la respuesta en un objeto JzavaScript (esto asume que el cuerpo de la respuesta esta en formato JSON).
 //Como funciona? el metodo .json() tambien devuelve una promesa, la cual se resuelve con el contenido del cuerpo de un formato JSON.
+then(data=>{
+    const menuContainer = document.getElementById('menu-container');
+    //Que hace? Aui se procesa los datos JSON ya convertidos. Se obtiene una referencia al contenido del menu en el HTML mediante getElementBYid ('menu-container'),y luego se itera sobre los elementos(caracteristicas)del menu.
+    data.items.forEach(category =>{
+        const categoryTitle = document.createElement('h2');
+        //
+    })
+})
